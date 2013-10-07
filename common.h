0d3bb3e615acc3c81ae9f7e75efdd3c15bbd0df5
Gerwin van der Lugt <gerwin3@gmail.com>
2013-10-07 20:51:53 +0200

    added min/max

---
+++ common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+extern inline int min (int a, int b)
+{
+	return (a < b ? a : b);
+}
+
+extern inline int max (int a, int b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
